22f85beddc0b8546bf25879c1296e4404f54e883
numasa <o.satoshi.0405@gmail.com>
2020-04-05 00:36:55 +0900

    fix redirect by indexOf

--- project/src/middleware/auth.ts
+++ project/src/middleware/auth.ts
@@ -1,13 +1,9 @@
 import { Middleware } from '@nuxt/types'
 import { auth } from '../plugins/firebase'
 
-const allowPage: string[] = ['/', '/login']
-
 const middleware:Middleware = ({ route, store, redirect }) => {
   auth.onAuthStateChanged((user) => {
-    if (! user && allowPage.indexOf(route.path) >= 0) {
-      redirect('/login')
-    }
+    if (! user && route.path !== '/login') redirect('/login')
   })
 }
 export default middleware
