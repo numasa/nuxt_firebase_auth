29506b4ef30d8a4d4600f280bee78819d57503f1
numasa <o.satoshi.0405@gmail.com>
2020-04-17 11:49:28 +0900

    fix header design

--- project/src/layouts/header.vue
+++ project/src/layouts/header.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app-bar app>
-    <v-toolbar-title><a href='/'>Sample Project</a></v-toolbar-title>
+    <v-toolbar-title>
+      <a href="/" class="header-title">
+      <svg class="NuxtLogo" width="30" height="30" viewBox="0 0 452 342" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" fill-rule="evenodd">
+          <path
+            d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
+            fill="#00C58E"
+          />
+          <path
+            d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
+            fill="#108775"
+          />
+          <path
+            d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
+            fill="#2F495E"
+            fill-rule="nonzero"
+          />
+        </g>
+      </svg>
+      project
+      </a>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
     <div v-if="loginUser">
       <v-menu bottom left>
@@ -29,14 +50,7 @@
       </v-menu>
     </div>
     <div v-else>
-      <v-row>
-        <v-col cols=6>
-          <div class="text-truncate my-3">未ログイン</div>
-        </v-col>
-        <v-col cols=6>
-          <v-btn class="button--green my-1" @click="login" align="right">ログインへ</v-btn>
-        </v-col>
-      </v-row>
+      <v-btn class="button--green my-1" @click="login" align="right">ログインへ</v-btn>
     </div>
   </v-app-bar>
 </template>
@@ -65,3 +79,11 @@ export default class DefaultHeader extends Vue {
   }
 }
 </script>
+
+<style>
+.header-title {
+  text-decoration: none;
+  color: #35495e !important;
+  display: flex;
+}
+</style>
--- project/src/middleware/auth.ts
+++ project/src/middleware/auth.ts
@@ -1,20 +1,14 @@
 import { Middleware } from '@nuxt/types'
 import { auth } from '../plugins/firebase'
 
-const middleware:Middleware = ({ route, store, redirect }) => {
+const middleware:Middleware = ({ route, redirect }) => {
   auth.onAuthStateChanged((user) => {
     if (! user && !route.path.startsWith('/login') ) {
       // 特定のPage以外はLogin画面へredirect
       // TopPage, not-members-only はredirectしない
-      console.log('in middleware:' + route.path)
       if ( route.path == '/' ) {}
       else if ( route.path.startsWith('/not-members-only') ) {}
-      else {
-        window.location.href = '/login'
-        return new Promise((resolve) => {
-          // Wait for broswer to redirect...
-        })
-      }
+      else return redirect('/login')
     }
   })
 }
