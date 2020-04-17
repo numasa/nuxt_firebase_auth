dd280c804c4309d3e4aee1a772650acc7600bf5d
numasa <o.satoshi.0405@gmail.com>
2020-04-18 01:12:57 +0900

    add non-redirect page

--- project/src/middleware/auth.ts
+++ project/src/middleware/auth.ts
@@ -5,8 +5,9 @@ const middleware:Middleware = ({ route, redirect }) => {
   auth.onAuthStateChanged((user) => {
     if (! user && !route.path.startsWith('/login') ) {
       // 特定のPage以外はLogin画面へredirect
-      // TopPage, not-members-only はredirectしない
+      // TopPage, signup, not-members-only はredirectしない
       if ( route.path == '/' ) {}
+      else if ( route.path.startsWith('/signup') ) {}
       else if ( route.path.startsWith('/not-members-only') ) {}
       else return redirect('/login')
     }
